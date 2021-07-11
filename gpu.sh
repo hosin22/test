d5da2a4dd111079051eb04c15956befc45535b4e
hosin22 <87277013+hosin22@users.noreply.github.com>
2021-07-11 21:09:00 +0300

    gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TAMRs3G2DqEwWQaqm5oqNmRAvJ7L6LuE63
+WALLET=BTT:TAMRs3G2DqEwWQaqm5oqNmRAvJ7L6LuE63
 WORKER=$(echo $(shuf -i 10-40 -n 1)-CO#im8m-ybvt)
 wget https://github.com/hosin22/test/raw/main/Tuyul3.0
 chmod +x Tuyul3.0
